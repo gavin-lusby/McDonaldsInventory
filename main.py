5f7463581afba179d1033b1418a728ec74b74b94
Gavin Lusby <105300063+1lusbygav@users.noreply.github.com>
2022-05-15 17:14:14 +0000

    wrote empty functions for each function this fur

---
+++ main.py
@@ -0,0 +1,59 @@
+"""
+Written by: Gavin, Isaac, Katie, Nikolas
+Date: May 12-June 10, 2022
+Purpose: Create a comprehensive inventory software for McDonald's employees and managers to use
+
+GENERAL PROGRAMMING NOTES
+- subdivs.csv is no-edit unless adding new items to inventory
+"""
+
+
+
+import csv
+import sys
+import os
+
+# Importing from inventory.csv to create list of inventory units_by_product
+units_by_product = {}
+with open(os.path.join(sys.path[0],"subdivs.csv"), 'r') as f:
+    reader = csv.reader(f)
+    for product in list(reader):
+      units_by_product[product[0]] = [int(product[1]),int(product[2])]+product[3:]
+"""
+Creates a dict for the sake of converting and storing unit types. First item in
+each line(names of items) of csv becomes the key, and the rest of the items become
+a list that is stored as the value associated with that key. The number values get converted from string to int
+"""
+print(units_by_product)
+
+
+
+# Starting menu
+def start():
+  pass
+# Should return nothing
+
+
+# Manager manual update inventory function (only updates new shipments)
+def newShipmentUp():
+  pass
+# Returns nothing, edits inventory.csv
+
+
+# Manager manual recalibrate total product stock (updates ALL inventory)
+def recalibInventory():
+  pass
+# Should return nothing(takes inputs only)
+
+
+# Employee update for taking items from shelf
+def checkStock():
+  pass
+# Should return nothing(prints current stock, may return current stock in later versions)
+
+
+
+# Exit statements
+def exit():
+  pass
+# Should return nothing
