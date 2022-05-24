7ac71c5abbb93d07d23a6c8eaec2789f77dcbc0f
Isaac Bisutti <>
2022-05-24 04:37:10 +0000

    just worked on checkstock

--- checkStock.py
+++ checkStock.py
@@ -1,8 +1,15 @@
 # Manager or employees check stock (how much has been used, how much needs to be ordered)
-def check_stock(item, stock):
+def DD_find (DD, item):
+  for l in range(len(DD)):
+    if DD[l] == item:
+      return l
+  pass
+
+def check_stock(item, stock, subdivs):
   if item == "all":
     for a in stock:
-      print(f"There are {a[1]} uses of {a[0]}.")
+      subdindex = DD_find(subdivs, a)
+      print(f"There are {a[1]//subdivs[0]} {a[0]} ({a[1] uses}).")
   else:
     for b in stock:
       if b[0] == item:
