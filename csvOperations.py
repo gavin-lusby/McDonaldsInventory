8d0710ae7df5894a4ee5d5f6160a1bb39218f949
1lusbygav <105300063+1lusbygav@users.noreply.github.com>
2022-05-26 10:37:48 -0400

    added unit conversion functions in csvOperations

--- csvOperations.py
+++ csvOperations.py
@@ -9,6 +9,7 @@ with open(ospath.join(syspath[0], "subdivs.csv"), 'r') as f:
         UBP[product[0]] = [int(product[1]), int(product[2])] + product[3:]
 
 
+# Fetches current inventory data and stores as dict in form of <product name> : <num of uses>
 def getInv():
     products = {}
     with open(ospath.join(syspath[0], "inventory.csv"), 'r') as f:
@@ -19,3 +20,31 @@ def getInv():
     return products
 
 
+# returns num of groups of product, and what a group is called(two decimal places)
+def useToGrp(product_name, uses):
+    return [(int(100 * uses / UBP[product_name][1])) / 100, UBP[product_name][3]]
+
+
+# returns num of containers of product, and what a container is called(two decimal places)
+def useToCon(product_name, uses):
+    return [(int(100 * (uses / UBP[product_name][1]) / UBP[product_name[0]])) / 100, UBP[product_name][2]]
+
+
+# returns num of uses of product
+def grpToUse(product_name, groups):
+    return groups * UBP[product_name][1]
+
+
+# returns num of uses of product
+def grpToCon(product_name, groups):
+    return [(int(100 * (groups / UBP[product_name][0]))), UBP[product_name][2]]
+
+
+# returns num of uses of product
+def conToUse(product_name, cons):
+    return cons * UBP[product_name][0] * UBP[product_name][1]
+
+
+# returns num of groups of product, and what a group is called
+def conToGrp(product_name, cons):
+    return [cons * UBP[product_name][0], UBP[product_name][3]]
