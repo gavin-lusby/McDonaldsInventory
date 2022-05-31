38bda71abea967aad5fdec0055cf750e65a936a9
IsaacBisutti <105300989+1bisuttiisa@users.noreply.github.com>
2022-05-31 14:02:33 +0000

    Getting Check Stock to work with dictionary

--- checkStock.py
+++ checkStock.py
@@ -6,38 +6,39 @@ from resetScreen import reset_screen
 # Edited by Isaac (2022-5-31)
 
 # used in checking
-def check(DD, item):
-  while True
-    if item.isalpha():
-      if item in DD:
-        break
-      else:
-        item = 
-    
-  
-  return item
+def check(Dict):
+    item = input("What item would you like to check: ").lower()
+    while True: #loops through until valid input
+        if item in Dict or item == 'all':
+            break
+        else:
+            item = input("\nItem not in inventory, please try again: ").lower()
+    return item
 
+def find (DD, item):
+    for i in DD:
+      if item == i[0]:
+        return DD.index(i)
+    
 def check_stock(stock, subdivs):
     reset_screen()
 
-    item = check(stock, input("What item would you like to check: ").lower())
+    item = check(subdivs)
 
     # if user wants to see all
     if item == "all":
         for a in subdivs:
-            subindex = 1
-            sub1 = stock[subindex][1] // a[2]  # Number of boxes
-            sub2 = stock[subindex][1] % a[2]  # Number of remaining uses
-            print(f"There are {sub1} {a[3]} and {sub2} {a[4]} of {a[0]} ({stock[subindex][1]} uses).")
+            index = find(stock, item)
+            sub1 = stock[index][1] // subdivs[a][2]
+            sub2 = stock[index][1] % subdivs[a][2]
+            print(f"There are {sub1} {a[3]} and {sub2} {a[4]} of {a[0]} ({stock[index][1]} uses).")
 
     # specific item
     else:
-        for b in subdivs:
-            if b == item:
-                subindex = 1
-                sub1 = stock[subindex][1] // b[2]  # Number of boxes
-                sub2 = stock[subindex][1] % b[2]  # Number of remaining uses
-                print(f"There are {sub1} {b[3]} and {sub2} {b[4]} of {b[0]} ({stock[subindex][1]} uses).")
+        index = find(stock, item)
+        sub1 = stock[index][1] // subdivs[item][2]  # Number of boxes
+        sub2 = stock[index][1] // subdivs[item][2]  # Number of remaining uses
+        print(f"There are {sub1} {subdivs[item][3]} and {sub2} {subdivs[item][4]} of {subdivs[item][0]} ({stock[index][1]} uses).")
 
     pass
 # Should return nothing(prints current stock, may return current stock in later versions)
